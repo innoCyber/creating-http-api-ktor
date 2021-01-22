07f6cbeaea0f0da7d7af5b7baffec3d64e58e7ca
innocent <innocentileka@gmail.com>
2021-01-22 13:27:54 +0100

    create route package and CustomerRoutes class

--- src/main/kotlin/com/jetbrains/handson/httpapi/models/Customer.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/models/Customer.kt
@@ -1,4 +1,9 @@
 package com.jetbrains.handson.httpapi.models
 
-class Customer {
+import kotlinx.serialization.Serializable
+
+@Serializable
+
+val customerStorage = mutableListOf<Customer>()
+class Customer( val id: String, val firstName: String, val lastName: String, val email: String) {
 }
---
+++ src/main/kotlin/com/jetbrains/handson/httpapi/routes/CustomerRoutes.kt
@@ -0,0 +1,4 @@
+package com.jetbrains.handson.httpapi.routes
+
+class CustomerRoutes {
+}
