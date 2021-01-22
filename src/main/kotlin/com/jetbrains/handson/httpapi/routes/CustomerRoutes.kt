5726d329f8db40cd9907a9da7ddca3c4ba4e2f0f
innocent <innocentileka@gmail.com>
2021-01-22 13:50:52 +0100

    install content negotiation

--- src/main/kotlin/com/jetbrains/handson/httpapi/routes/CustomerRoutes.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/routes/CustomerRoutes.kt
@@ -1,4 +1,25 @@
 package com.jetbrains.handson.httpapi.routes
 
-class CustomerRoutes {
+import com.jetbrains.handson.httpapi.models.customerStorage
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+
+fun Route.customerRoutes() {
+    route("/customer"){
+        get{}
+        get("{id}"){}
+        post {  }
+        delete { "{id}" }
+    }
+
+
+    get{
+        if(customerStorage.isNotEmpty()){
+            call.respond(customerStorage)
+        }else{
+            call.respondText("Customer list empty", status = HttpStatusCode.NotFound)
+        }
+    }
 }
