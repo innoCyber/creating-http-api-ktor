39a31d9bad598a015e98e5020043c6553d03c5bd
innocent <innocentileka@gmail.com>
2021-01-22 13:53:16 +0100

    install content negotiation

--- src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
@@ -2,12 +2,13 @@ package com.jetbrains.handson.httpapi
 
 import io.ktor.application.*
 import io.ktor.features.*
-import kotlinx.serialization.json.json
+import io.ktor.serialization.*
 
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
-install(ContentNegotiation)
-    json()
+    install(ContentNegotiation) {
+        json()
+    }
 }
